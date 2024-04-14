0d5a7277876666a90f5f933cad7733b9edd15473
Antonio Curavalea <one.kyonblack@gmail.com>
2024-04-14 23:33:06 +0300

    build: move to PyInstaller, use UPX

--- build_exec.bat
+++ build_exec.bat
@@ -1,3 +1,2 @@
 pip install -r requirements.txt
-pip install --upgrade --pre --extra-index-url https://marcelotduarte.github.io/packages/ cx_Freeze
-cxfreeze -O -c starmap.py --include-files ./assets/ --target-dir dist
+pyinstaller --onefile --strip --hide-console --optimize 2 starmap.py
